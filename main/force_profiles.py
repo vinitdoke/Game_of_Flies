ddeda4db77211cc32d1f64d5fdf7577efd57a2d2
TanyutSharma <65652483+TanyutSharma@users.noreply.github.com>
2023-03-12 18:54:32 +0530

    Work allocation

---
+++ main/force_profiles.py
@@ -0,0 +1,9 @@
+def all_force_functions(
+        profile_name : str,
+        *args
+
+)   
+    """
+    return a matrix of functions for the Clusters Case
+    """
+    pass
---
+++ main/state_parameters.py
@@ -0,0 +1,12 @@
+def initialise(
+        input_list : list,
+
+):
+    """
+    Outputs:
+    1. Positions (pos_x, pos_y, pos_z)
+    2. Velocities (vel_x, vel_y, vel_z)
+    3. Type-Indices
+    4. Parameter_matrix (n_type*n_type*4 array)
+    """
+    pass
