902c95c4b18ff95a0134c78f6ac3159ee36311dc
vinitdoke <54439587+vinitdoke@users.noreply.github.com>
2023-03-12 19:32:08 +0530

    Corrected parameter_matrix specifications

--- main/state_parameters.py
+++ main/state_parameters.py
@@ -7,6 +7,8 @@ def initialise(
     1. Positions (pos_x, pos_y, pos_z)
     2. Velocities (vel_x, vel_y, vel_z)
     3. Type-Indices
-    4. Parameter_matrix (n_type*n_type*4 array)
+    4. Parameter_matrix (n_type*n_type*3 (r_max, r_min, f_max) array)
+       r_min < r_max
+       0<= f_max <= 1
     """
     pass
