f173cd8e5da780a87438ffdf27ba0d3d8231d35f
TanyutSharma <65652483+TanyutSharma@users.noreply.github.com>
2023-03-15 13:04:36 +0530

    boilerplate

--- main/acceleration_updater.py
+++ main/acceleration_updater.py
@@ -1,1 +1,26 @@
-def interaction
+import force_profiles
+
+print(force_profiles.wrap_clusters_force_distance)
+#def interaction
+
+def accelerator(
+        
+):  
+    """
+    INPUTS
+    1. matrix_of_force_functions
+    2. state_variable_dict : dict
+        {
+        "pos_x"  : array,
+        "pos_y"  : array,
+        "pos_z"  : array,
+        "vel_x"  : array,
+        "vel_y"  : array,
+        "vel_z"  : array,
+        "limits" : tuple (min, max),
+        "n_type_array" : ndarray,
+        
+
+        }
+    """
+    pass
