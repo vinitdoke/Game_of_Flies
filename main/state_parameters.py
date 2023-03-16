2bfb73f1a7961932bb69135f7859b01d26bc60cb
vinitdoke <54439587+vinitdoke@users.noreply.github.com>
2023-03-16 19:03:30 +0530

    pycharm reformatting

--- main/state_parameters.py
+++ main/state_parameters.py
@@ -33,29 +33,30 @@ def _plot_dist(pos_x, pos_y, pos_z,
                max_particles, len_p_tynum):
     plt.clf()
     for i in range(len_p_tynum):
-        plt.scatter(pos_x[i*max_particles: (i+1)*max_particles],
-                    pos_y[i*max_particles: (i+1)*max_particles], label = f"Particle {i}")
+        plt.scatter(pos_x[i * max_particles: (i + 1) * max_particles],
+                    pos_y[i * max_particles: (i + 1) * max_particles],
+                    label=f"Particle {i}")
     plt.xlim(-1, 101)
     plt.ylim(-1, 101)
     plt.title('Particle Distribution')
-    #plt.legend()
+    # plt.legend()
     plt.draw()
     plt.pause(0.1)
 
 
-def initialise(n_type, *params): #p_tnum -- n_type
+def initialise(n_type, *params):  # p_tnum -- n_type
     max_particles = 1000  # max particle of a single kind
     n_params = len(params)
     len_p_tynum = len(n_type)
-    dist_params = int(n_params/(len_p_tynum*len_p_tynum))
+    dist_params = int(n_params / (len_p_tynum * len_p_tynum))
 
-    pos_x = np.empty(len_p_tynum*max_particles)*np.NAN
-    pos_y = np.empty(len_p_tynum*max_particles)*np.NAN
-    pos_z = np.empty(len_p_tynum*max_particles)*np.NAN
+    pos_x = np.empty(len_p_tynum * max_particles) * np.NAN
+    pos_y = np.empty(len_p_tynum * max_particles) * np.NAN
+    pos_z = np.empty(len_p_tynum * max_particles) * np.NAN
 
-    vel_x = np.empty(len_p_tynum*max_particles)*np.NAN
-    vel_y = np.empty(len_p_tynum*max_particles)*np.NAN
-    vel_z = np.empty(len_p_tynum*max_particles)*np.NAN
+    vel_x = np.empty(len_p_tynum * max_particles) * np.NAN
+    vel_y = np.empty(len_p_tynum * max_particles) * np.NAN
+    vel_z = np.empty(len_p_tynum * max_particles) * np.NAN
 
     interact_matrix = np.zeros((dist_params, len_p_tynum, len_p_tynum))
 
@@ -65,33 +66,32 @@ def initialise(n_type, *params): #p_tnum -- n_type
     """
     for i in range(len_p_tynum):
         n_specp = int(n_type[i])
-        x = 100.0*random.rand(n_specp)
-        y = 100.0*random.rand(n_specp)
-        z = 100.0*random.rand(n_specp)
-        pos_x[i*max_particles: i*max_particles + n_specp] = x[:]
-        pos_y[i*max_particles: i*max_particles + n_specp] = y[:]
-        pos_z[i*max_particles: i*max_particles + n_specp] = z[:]
-        v_x = 2*random.rand(n_specp) - 1
-        v_y = 2*random.rand(n_specp) - 1
-        v_z = 2*random.rand(n_specp) - 1
-        vel_x[i*max_particles: i*max_particles + n_specp] = v_x
-        vel_y[i*max_particles: i*max_particles + n_specp] = v_y
-        vel_z[i*max_particles: i*max_particles + n_specp] = v_z
+        x = 100.0 * random.rand(n_specp)
+        y = 100.0 * random.rand(n_specp)
+        z = 100.0 * random.rand(n_specp)
+        pos_x[i * max_particles: i * max_particles + n_specp] = x[:]
+        pos_y[i * max_particles: i * max_particles + n_specp] = y[:]
+        pos_z[i * max_particles: i * max_particles + n_specp] = z[:]
+        v_x = 2 * random.rand(n_specp) - 1
+        v_y = 2 * random.rand(n_specp) - 1
+        v_z = 2 * random.rand(n_specp) - 1
+        vel_x[i * max_particles: i * max_particles + n_specp] = v_x
+        vel_y[i * max_particles: i * max_particles + n_specp] = v_y
+        vel_z[i * max_particles: i * max_particles + n_specp] = v_z
 
     for k in range(dist_params):
         for i in range(len_p_tynum):
             for j in range(len_p_tynum):
                 interact_matrix[k, i, j] = params[
-                    len_p_tynum*len_p_tynum*k + len_p_tynum*i + j
+                    len_p_tynum * len_p_tynum * k + len_p_tynum * i + j
                     ]
 
-    #_plot_dist(pos_x, pos_y, pos_z, max_particles, len_p_tynum)
+    # _plot_dist(pos_x, pos_y, pos_z, max_particles, len_p_tynum)
 
     return pos_x, pos_y, pos_z, vel_x, vel_y, vel_z, interact_matrix, max_particles
 
 
 if __name__ == '__main__':
-
     p_x, p_y, p_z, v_x, v_y, v_z, inter = initialise([30, 40, 20],
                                                      1, 2, 3,
                                                      4, 5, 6,
