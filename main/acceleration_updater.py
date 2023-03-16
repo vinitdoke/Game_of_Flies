ed1bddb41e1945aa614c462e43c70ed77d558699
vinitdoke <54439587+vinitdoke@users.noreply.github.com>
2023-03-16 19:01:52 +0530

    pycharm reformatting

--- main/acceleration_updater.py
+++ main/acceleration_updater.py
@@ -10,10 +10,11 @@ import time
 lim = 0
 a = 5
 
+
 @numba.njit
 def resolve_particle_group(
         force_function,
-        index_1: int, # the indices of the particle types to be resolved
+        index_1: int,  # the indices of the particle types to be resolved
         index_2: int,
         pos_x: np.ndarray,
         pos_y: np.ndarray,
@@ -22,13 +23,13 @@ def resolve_particle_group(
         vel_y: np.ndarray,
         vel_z: np.ndarray,
         limits: tuple,
-        r_max :float, # max distance at which particles interact
+        r_max: float,  # max distance at which particles interact
         n_type_array: np.ndarray,
         max_particles_per_type: int,
 
         acc_x: np.ndarray,
         acc_y: np.ndarray,
-        acc_z: np.ndarray # to avoid allocating inside function
+        acc_z: np.ndarray  # to avoid allocating inside function
 ) -> None:
     for i1 in range(n_type_array[index_1]):
         p1 = max_particles_per_type * index_1 + i1
@@ -40,9 +41,10 @@ def resolve_particle_group(
                 pos_x_2 = pos_x[p2]
                 pos_y_2 = pos_y[p2]
 
-                distance = (pos_x_1 - pos_x_2) * (pos_x_1 - pos_x_2) + (pos_y_1 - pos_y_2) * (pos_y_1 - pos_y_2)
+                distance = (pos_x_1 - pos_x_2) * (pos_x_1 - pos_x_2) + (
+                            pos_y_1 - pos_y_2) * (pos_y_1 - pos_y_2)
 
-                if 1e-10 < distance < r_max * r_max: # 1e-10 is r_min**2
+                if 1e-10 < distance < r_max * r_max:  # 1e-10 is r_min**2
                     distance = np.sqrt(distance)
                     acc = force_function(distance)
                     a_x = acc * (pos_x_1 - pos_x_2) / distance
@@ -61,6 +63,7 @@ def resolve_particle_group(
                     elif pos_y_1 > 100 - lim:
                         acc_y[p1] -= a
 
+
 def accelerator(
         matrix_of_functions: list,
         pos_x: np.ndarray,
@@ -70,13 +73,13 @@ def accelerator(
         vel_y: np.ndarray,
         vel_z: np.ndarray,
         limits: tuple,
-        r_max :float, # max distance at which particles interact
+        r_max: float,  # max distance at which particles interact
         n_type_array: np.ndarray,
         max_particles_per_type: int,
 
         acc_x: np.ndarray,
         acc_y: np.ndarray,
-        acc_z: np.ndarray # to avoid allocating inside function
+        acc_z: np.ndarray  # to avoid allocating inside function
 ) -> None:
     """
     INPUTS
@@ -102,7 +105,10 @@ def accelerator(
     for j1 in range(num_particle_types):
         for j2 in range(num_particle_types):
             force_function = matrix_of_functions[j1][j2]
-            resolve_particle_group(force_function, j1, j2, pos_x, pos_y, pos_z, vel_x, vel_y, vel_z, (100, 100), 10, n_type_array, max_particles_per_type, acc_x, acc_y, acc_z)
+            resolve_particle_group(force_function, j1, j2, pos_x, pos_y, pos_z,
+                                   vel_x, vel_y, vel_z, (100, 100), 10,
+                                   n_type_array, max_particles_per_type, acc_x,
+                                   acc_y, acc_z)
 
 
 if __name__ == "__main__":
@@ -115,19 +121,22 @@ if __name__ == "__main__":
 
     n_type_arr = np.array([1000, 1000, 1000])
 
-    pos_x, pos_y, pos_z, vel_x, vel_y, vel_z, interact_matrix, max_particles = initialise(n_type_arr)
+    pos_x, pos_y, pos_z, vel_x, vel_y, vel_z, interact_matrix, max_particles = initialise(
+        n_type_arr)
 
-    #_plot_dist(pos_x, pos_y, pos_z, max_particles, 3)
+    # _plot_dist(pos_x, pos_y, pos_z, max_particles, 3)
 
     acc_x = np.zeros_like(vel_x)
     acc_y = np.zeros_like(vel_x)
     acc_z = np.zeros_like(vel_x)
 
-    accelerator(mof, pos_x, pos_y, pos_z, vel_x, vel_y, vel_z, (100, 100), 10, n_type_arr, max_particles, acc_x, acc_y, acc_z)
+    accelerator(mof, pos_x, pos_y, pos_z, vel_x, vel_y, vel_z, (100, 100), 10,
+                n_type_arr, max_particles, acc_x, acc_y, acc_z)
 
     start = time.perf_counter()
     for i in range(10):
-        accelerator(mof, pos_x, pos_y, pos_z, vel_x, vel_y, vel_z, (100, 100), 10, n_type_arr, max_particles, acc_x, acc_y, acc_z)
+        accelerator(mof, pos_x, pos_y, pos_z, vel_x, vel_y, vel_z, (100, 100),
+                    10, n_type_arr, max_particles, acc_x, acc_y, acc_z)
     start = time.perf_counter() - start
 
     print(f"Approx FPS: {10 / start}")
--- main/integrator.py
+++ main/integrator.py
@@ -9,7 +9,8 @@ import matplotlib.pyplot as plt
 
 fac = 0.92
 
-#@numba.njit
+
+# @numba.njit
 def integrate(
         mof: list,
         pos_x: np.ndarray,
@@ -19,7 +20,7 @@ def integrate(
         vel_y: np.ndarray,
         vel_z: np.ndarray,
         limits: tuple,
-        r_max :float, # max distance at which particles interact
+        r_max: float,  # max distance at which particles interact
         n_type_array: np.ndarray,
         max_particles_per_type: int,
 
@@ -32,23 +33,24 @@ def integrate(
     Inputs :
     
     """
-    accelerator(mof, pos_x, pos_y, pos_z, vel_x, vel_y, vel_z, limits, r_max, n_type_array, max_particles_per_type, acc_x, acc_y, acc_z)
+    accelerator(mof, pos_x, pos_y, pos_z, vel_x, vel_y, vel_z, limits, r_max,
+                n_type_array, max_particles_per_type, acc_x, acc_y, acc_z)
 
     pos_x += vel_x * 0.5 * timestep
     pos_y += vel_y * 0.5 * timestep
-    #pos_z += vel_z * 0.5 * timestep
+    # pos_z += vel_z * 0.5 * timestep
 
     vel_x += acc_x * timestep
     vel_y += acc_y * timestep
-    #vel_z += acc_z * timestep
+    # vel_z += acc_z * timestep
 
     vel_x *= fac
     vel_y *= fac
-    #vel_z *= fac
+    # vel_z *= fac
 
     pos_x += vel_x * 0.5 * timestep
     pos_y += vel_y * 0.5 * timestep
-    #pos_z += vel_z * 0.5 * timestep
+    # pos_z += vel_z * 0.5 * timestep
 
     pos_x[pos_x < 0] += 100
     pos_x[pos_x > 100] -= 100
@@ -56,42 +58,44 @@ def integrate(
     pos_y[pos_y < 0] += 100
     pos_y[pos_y > 100] -= 100
 
-n_type = 3
-sample_input = np.random.randint(5, 10, (n_type, n_type, 4))
-sample_input[:, :, 2] *= -1
-sample_input[:, :, 0] = sample_input[:, :, 1] - 2
 
-mof = all_force_functions("cluster_distance_input", *sample_input)
+if __name__ == "__main__":
+    n_type = 3
+    sample_input = np.random.randint(5, 10, (n_type, n_type, 4))
+    sample_input[:, :, 2] *= -1
+    sample_input[:, :, 0] = sample_input[:, :, 1] - 2
 
-n_type_arr = np.array([1000, 1000, 1000])
+    mof = all_force_functions("cluster_distance_input", *sample_input)
 
-pos_x, pos_y, pos_z, vel_x, vel_y, vel_z, interact_matrix, max_particles = initialise(n_type_arr)
+    n_type_arr = np.array([1000, 1000, 1000])
 
-vel_x = np.zeros_like(pos_x)
-vel_y = np.zeros_like(pos_x)
-vel_z = np.zeros_like(pos_x)
+    pos_x, pos_y, pos_z, vel_x, vel_y, vel_z, interact_matrix, max_particles = initialise(
+        n_type_arr)
 
+    vel_x = np.zeros_like(pos_x)
+    vel_y = np.zeros_like(pos_x)
+    vel_z = np.zeros_like(pos_x)
 
-acc_x = np.zeros_like(vel_x)
-acc_y = np.zeros_like(vel_x)
-acc_z = np.zeros_like(vel_x)
+    acc_x = np.zeros_like(vel_x)
+    acc_y = np.zeros_like(vel_x)
+    acc_z = np.zeros_like(vel_x)
 
-output = []
+    output = []
 
-plt.figure(figsize=(12, 12), dpi=80)
-for i in range(10000):
-    #out = np.vstack([pos_x, pos_y]).T
-    #output.append(out)
-    dt = np.sqrt(np.max(vel_x * vel_x + vel_y * vel_y))
-    
-    if dt > 1e-15:
-        dt = 0.4 / dt
-    else:
-        dt = 0.1
-    
+    plt.figure(figsize=(12, 12), dpi=80)
+    for i in range(10000):
+        # out = np.vstack([pos_x, pos_y]).T
+        # output.append(out)
+        dt = np.sqrt(np.max(vel_x * vel_x + vel_y * vel_y))
 
-    integrate(mof, pos_x, pos_y, pos_z, vel_x, vel_y, vel_z, (100, 100), 10, n_type_arr, max_particles, acc_x, acc_y, acc_z, dt)
-    if i % 1 == 0:
-        _plot_dist(pos_x, pos_y, pos_z, max_particles, 3)
+        if dt > 1e-15:
+            dt = 0.4 / dt
+        else:
+            dt = 0.1
 
-#np.savez("vid", result = output)
+        integrate(mof, pos_x, pos_y, pos_z, vel_x, vel_y, vel_z, (100, 100),
+                  10, n_type_arr, max_particles, acc_x, acc_y, acc_z, dt)
+        if i % 1 == 0:
+            _plot_dist(pos_x, pos_y, pos_z, max_particles, 3)
+
+    # np.savez("vid", result = output)
