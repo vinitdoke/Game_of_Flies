66405f02182f2258115ef0372f2954591921c94f
vinitdoke <54439587+vinitdoke@users.noreply.github.com>
2023-03-28 00:02:06 +0530

    added parser for blind_run argument

--- main/hive.py
+++ main/hive.py
@@ -1,5 +1,6 @@
 from simulation import Simulation
 from vizman import *
+from vispy import app
 
 from argparse import ArgumentParser
 
@@ -13,7 +14,7 @@ def parse():
 if __name__ == "__main__":
 
 	args = parse()
-	input_array = np.array([10]*9)  # max 9 types due to color_list
+	input_array = np.array([100]*9)  # max 9 types due to color_list
 	simulation = Simulation(input_array, seed = 1234, limits=(100, 100, 100))
 	simulation.update()  # dummy call to avoid frame freeze on first update
 	# seed 4, 10, 100, 50, 69, 35, 434, 954, 1039
@@ -23,8 +24,10 @@ if __name__ == "__main__":
 		visual.set_simulation_instance(simulation)
 		visual.draw_boundary()
 		visual.set_axis()
+		visual.print_fps = True
 
 		visual.start()
+		app.run()
 	
 	else:
 		simulation.blind_run(1000)
