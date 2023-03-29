82c529ff47d8a80d3f32b0c6c41bbace0a6dfd5f
vinitdoke <54439587+vinitdoke@users.noreply.github.com>
2023-03-29 17:37:55 +0530

    new kernels.py file for all cuda kernels and device functions to be written

---
+++ main/kernels.py
@@ -0,0 +1,18 @@
+from numba import cuda
+import numpy as np
+
+"""
+Tutorials:
+https://nyu-cds.github.io/python-numba/05-cuda/
+
+Documentation:
+https://numba.pydata.org/numba-doc/latest/cuda/index.html
+
+Kernel List:
+
+Device Functions:
+
+"""
+
+if __name__ == "__main__":
+    print(cuda.gpus)
